439f70c0a136fb8e42618e311f1ad73c78c67953
EssuthiAnge <mbangueange@gmail.com>
2026-02-05 17:22:28 +0100

    Ajout solution P1C3 : affichage et calcul simple

---
+++ P1/P1C3/main.py
@@ -0,0 +1,14 @@
+"""
+Exercice P1C3 – Bases de Python
+
+Objectifs :
+- Afficher un message avec print
+- Effectuer un calcul simple et afficher le résultat
+"""
+
+# Affichage d'un message
+print("J'apprends Python !")
+
+# Calcul et affichage d'une expression arithmétique
+print(17 + 35 * 2)
+
