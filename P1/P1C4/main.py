bf22083fa1d9a73e340151ffa87aec6e0360986d
EssuthiAnge <mbangueange@gmail.com>
2026-02-05 17:33:28 +0100

    Ajout solution P1C4 : variables et f-strings

---
+++ P1/P1C4/main.py
@@ -0,0 +1,22 @@
+"""
+Exercice P1C4 – Variables et f-strings
+
+Objectifs :
+- Créer et utiliser des variables
+- Afficher des informations formatées
+- Modifier la valeur d'une variable
+"""
+
+# Déclaration des variables
+nom = "Essuthi Mbangué"
+age = 26
+
+# Affichage des informations initiales
+print(f"Je m'appelle {nom} et j'ai {age} ans.")
+
+# Mise à jour de l'âge
+age += 10
+
+# Affichage après modification
+print(f"Je m'appelle {nom} et j'ai {age} ans maintenant.")
+
